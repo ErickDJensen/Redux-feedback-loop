c1dd1d61ab57e177a338d5d838442ec282f225c6
Erick Jensen <Erick.Jensen010@gmail.com>
2020-04-10 16:05:23 -0500

    setup routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import {HashRouter as Router, Route} from 'react-router-dom'
+// import axios from 'axios';
 import './App.css';
+import {withRouter} from 'react-router'
+
+import Page1 from '../Pages/Page1/Page1';
+import Page2 from '../Pages/Page2/Page2';
+import Page3 from '../Pages/Page3/Page3';
+import Page4 from '../Pages/Page4/Page4';
+
 
 class App extends Component {
   render() {
@@ -10,10 +18,16 @@ class App extends Component {
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
+        <Router>
+          <Route exact path='/' component={Page1}/>
+          <Route path='/Page2' component={Page2}/>
+          <Route path='/Page3' component={Page3}/>
+          <Route path='/Page3' component={Page4}/>
+        </Router>
         <br/>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
